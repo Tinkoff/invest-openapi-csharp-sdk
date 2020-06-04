b4284d1bd81625a7e30ea3980b409a006201924a
Oleg Shevchenko <olsh.me@gmail.com>
2020-06-04 23:07:25 +0300

    Add time property to streaming responses

--- src/Tinkoff.Trading.OpenApi/Models/StreamingResponse.cs
+++ src/Tinkoff.Trading.OpenApi/Models/StreamingResponse.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Newtonsoft.Json;
 
 namespace Tinkoff.Trading.OpenApi.Models
@@ -6,15 +8,18 @@ namespace Tinkoff.Trading.OpenApi.Models
     public abstract class StreamingResponse
     {
         [JsonIgnore] public abstract string Event { get; }
+
+        public DateTime Time { get; protected set; }
     }
 
     public abstract class StreamingResponse<T> : StreamingResponse
     {
         public T Payload { get; }
 
-        protected StreamingResponse(T payload)
+        protected StreamingResponse(T payload, DateTime time)
         {
             Payload = payload;
+            Time = time;
         }
     }
 }
---
+++ tests/Tinkoff.Trading.OpenApi.Tests/StreamingResponseTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using FluentAssertions;
+
+using Newtonsoft.Json;
+
+using Tinkoff.Trading.OpenApi.Models;
+using Tinkoff.Trading.OpenApi.Tests.TestHelpers;
+
+using Xunit;
+
+namespace Tinkoff.Trading.OpenApi.Tests
+{
+    public class StreamingResponseTests
+    {
+        [Fact]
+        public void DeserializeCandleTest()
+        {
+            var streamingResponse = JsonConvert.DeserializeObject<StreamingResponse>(JsonFile.Read("streaming-candle-response"));
+            var response = streamingResponse as StreamingResponse<CandlePayload>;
+
+            var expectedResponse = new CandleResponse(
+                new CandlePayload(
+                    64.0575m,
+                    64.0578m,
+                    64.0579m,
+                    64.0573m,
+                    156,
+                    new DateTime(2019, 08, 07, 15, 35, 00, DateTimeKind.Utc),
+                    CandleInterval.FiveMinutes,
+                    "BBG0013HGFT4"),
+                new DateTime(2019, 08, 07, 15, 35, 01, 029, DateTimeKind.Utc).AddTicks(7213));
+
+            response.Should().BeEquivalentTo(expectedResponse);
+        }
+    }
+}
